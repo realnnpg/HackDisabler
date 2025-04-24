a428e84fe39524a086bd5b368cbbfcf796f85a2c
realnnpg <charis.giannitsaros@gmail.com>
2025-04-24 12:40:27 +0300

    fixed the bug

--- src/main/java/com/nnpg/hackdisabler/modules/HackDisabler.java
+++ src/main/java/com/nnpg/hackdisabler/modules/HackDisabler.java
@@ -1,4 +1,5 @@
 package com.nnpg.hackdisabler.modules;
+
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.settings.ModuleListSetting;
@@ -11,17 +12,21 @@ import meteordevelopment.meteorclient.events.game.GameLeftEvent;
 import meteordevelopment.meteorclient.events.entity.player.PlayerMoveEvent;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
-import net.fabricmc.loader.impl.util.log.Log;
+
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 public class HackDisabler extends Module {
     private final Set<Module> disabledModules = new HashSet<>();
     private final Set<Module> queuedModules = new HashSet<>();
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     private final Set<Module> trackedModules = new HashSet<>();
+    private final Set<Module> disabledModulesOnDisconnect = new HashSet<>();
+
+
     public enum ModuleActivationBehavior {
         Allow,
         Queue,
@@ -41,38 +46,54 @@ public class HackDisabler extends Module {
         .defaultValue(ModuleActivationBehavior.Block)
         .build()
     );
+
     public HackDisabler() {
         super(Categories.Misc, "Hack Disabler", "Disables all active Meteor hacks and re-enables them when turned off.");
         MeteorClient.EVENT_BUS.subscribe(this);
     }
+
     @Override
     public void onActivate() {
-        MeteorClient.LOG.warn("nnpg-Entered onActivate");
-        disabledModules.clear();
-        queuedModules.clear();
-        trackedModules.clear();
-        List<Module> ignoredModules = ignoredModulesSetting.get();
-        for (Module module : Modules.get().getAll()) {
-            if (module != this) {
-                trackedModules.add(module);
-                if (module.isActive() && !ignoredModules.contains(module)) {
-                    module.toggle();
-                    disabledModules.add(module);
+        if (!disabledModulesOnDisconnect.isEmpty()) {
+            List<Module> ignoredModules = ignoredModulesSetting.get();
+            for (Module module : disabledModulesOnDisconnect) {
+                if (module != this) {
+                    trackedModules.add(module);
+                    if (!ignoredModules.contains(module)) {
+                        disabledModules.add(module);
+                    }
                 }
             }
+            disabledModulesOnDisconnect.clear();
+        } else {
+
+            disabledModules.clear();
+            queuedModules.clear();
+            trackedModules.clear();
+            List<Module> ignoredModules = ignoredModulesSetting.get();
+            for (Module module : Modules.get().getAll()) {
+                if (module != this) {
+                    trackedModules.add(module);
+                    if (module.isActive() && !ignoredModules.contains(module)) {
+                        module.toggle();
+                        disabledModules.add(module);
+                    }
+                }
+            }
+            info("Disabled " + disabledModules.size() + " modules.");
         }
-        info("Disabled " + disabledModules.size() + " modules.");
     }
+
+
     @Override
     public void onDeactivate() {
-        MeteorClient.LOG.warn("nnpg-Entered onDeactivate");
         for (Module module : disabledModules) {
             if (!module.isActive()) {
                 module.toggle();
             }
         }
         info("Re-enabled " + disabledModules.size() + " modules.");
-        disabledModules.clear();
+        //disabledModules.clear();
         for (Module module : queuedModules) {
             if (!module.isActive()) {
                 module.toggle();
@@ -84,14 +105,16 @@ public class HackDisabler extends Module {
         queuedModules.clear();
         trackedModules.clear();
     }
+
+
     @EventHandler
     private void onGameLeft(GameLeftEvent event) {
-        MeteorClient.LOG.warn("nnpg-Entered onGameLefet");
-
         List<Module> ignoredModules = ignoredModulesSetting.get();
         for (Module module : Modules.get().getAll()) {
             if (module != this && module.isActive() && !ignoredModules.contains(module)) {
                 module.toggle();
+                disabledModulesOnDisconnect.add(module);
+
             }
         }
         info("Disabled all modules (server disconnect).");
@@ -122,3 +145,4 @@ public class HackDisabler extends Module {
         }
     }
 }
+
